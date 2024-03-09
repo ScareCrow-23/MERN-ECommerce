65758514666adec476531391131f6e8aa944f08c
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-03-09 18:50:37 +0530

    Shipping Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { lazy, Suspense } from "react"
-import Loader from "./components/loader"
-import Header from "./components/header"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Loader from "./components/loader";
+import Header from "./components/header";
 
-const Home = lazy(() =>import("./pages/home"))
-const Search =lazy(() =>import("./pages/search"))
-const Cart = lazy(() =>import("./pages/cart"))
-
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const Cart = lazy(() => import("./pages/cart"));
+const Shipping = lazy(() => import("./pages/shipping"));
 
 // Admin routes Imports --------------------------------
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -27,50 +27,54 @@ const TransactionManagement = lazy(
   () => import("./pages/admin/management/transactionmanagement")
 );
 
-
 const App = () => {
   return (
     <Router>
       {/* Header */}
-      <Header/>
-     <Suspense fallback={<Loader/>}>
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/cart" element={<Cart />} />
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+          {/* Logged In User Routes */}
+          <Route>
+            <Route path="/shipping" element={<Shipping />} />
+          </Route>
+          {/* Admin Routes */}
+          <Route
+          // element={
+          //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
+          // }
+          >
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/product" element={<Products />} />
+            <Route path="/admin/customer" element={<Customers />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+            {/* Charts */}
+            <Route path="/admin/chart/bar" element={<Barcharts />} />
+            <Route path="/admin/chart/pie" element={<Piecharts />} />
+            <Route path="/admin/chart/line" element={<Linecharts />} />
+            {/* Apps */}
+            <Route path="/admin/app/coupon" element={<Coupon />} />
+            <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+            <Route path="/admin/app/toss" element={<Toss />} />
 
-        {/* Admin Routes */}
-        <Route
-  // element={
-  //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
-  // }
->
-  <Route path="/admin/dashboard" element={<Dashboard />} />
-  <Route path="/admin/product" element={<Products />} />
-  <Route path="/admin/customer" element={<Customers />} />
-  <Route path="/admin/transaction" element={<Transaction />} />
-  {/* Charts */}
-  <Route path="/admin/chart/bar" element={<Barcharts />} />
-  <Route path="/admin/chart/pie" element={<Piecharts />} />
-  <Route path="/admin/chart/line" element={<Linecharts />} />
-  {/* Apps */}
-  <Route path="/admin/app/coupon" element={<Coupon />} />
-  <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
-  <Route path="/admin/app/toss" element={<Toss />} />
+            {/* Management */}
+            <Route path="/admin/product/new" element={<NewProduct />} />
 
-  {/* Management */}
-  <Route path="/admin/product/new" element={<NewProduct />} />
+            <Route path="/admin/product/:id" element={<ProductManagement />} />
 
-  <Route path="/admin/product/:id" element={<ProductManagement />} />
-
-  <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
-</Route>;
-
-
-      </Routes>
-     </Suspense>
+            <Route
+              path="/admin/transaction/:id"
+              element={<TransactionManagement />}
+            />
+          </Route>
+          ;
+        </Routes>
+      </Suspense>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
